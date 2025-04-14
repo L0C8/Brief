f2f7efbd44c24691f7e6b82cf1c77fdb97e28edf
L0C8 <TimB02@protonmail.com>
2025-04-15 01:34:56 +0200

    added test gui

--- gui.py
+++ gui.py
@@ -1,3 +1,47 @@
-from themes import current_theme, toggle_theme, set_theme_by_name
+import tkinter as tk
+from tkinter import ttk
+from themes import get_current_theme, list_themes, switch_to_theme
 
-# we need a gui, lol
+class BriefApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Brief")
+        self.geometry("240x500")
+        self.resizable(False, False)
+        self.theme_names = list_themes()
+        self.build_ui()
+
+    def build_ui(self):
+        theme = get_current_theme()
+        self.apply_theme(theme)
+
+        self.dropdown_label = tk.Label(self, text="Select Theme:")
+        self.dropdown_label.pack(pady=(20, 5))
+
+        self.theme_var = tk.StringVar(value=self.theme_names[0])
+        self.dropdown = ttk.Combobox(self, values=self.theme_names, textvariable=self.theme_var, state="readonly")
+        self.dropdown.pack(pady=(0, 20))
+        self.dropdown.bind("<<ComboboxSelected>>", self.on_theme_change)
+
+        self.update_widget_styles(theme)
+
+    def on_theme_change(self, event):
+        selected_name = self.theme_var.get()
+        index = self.theme_names.index(selected_name)
+        theme = switch_to_theme(index)
+        self.apply_theme(theme)
+
+    def apply_theme(self, theme):
+        self.bg = theme["bg"]
+        self.fg = theme["fg"]
+        self.configure(bg=self.bg)
+        self.update_widget_styles(theme)
+
+    def update_widget_styles(self, theme):
+        if hasattr(self, "dropdown_label"):
+            self.dropdown_label.config(bg=theme["bg"], fg=theme["fg"])
+        self.configure(bg=theme["bg"])
+
+if __name__ == "__main__":
+    app = BriefApp()
+    app.mainloop()
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-from utils import initialize_default_theme_file
-# from gui import BriefApp
+from utils import initialize_default_theme_file, initialize_user_settings
+from gui import BriefApp
 
 os.makedirs("data", exist_ok=True)
 initialize_default_theme_file()
+initialize_user_settings()
 
-# if __name__ == "__main__":
-    # app = BriefApp()
-    # app.mainloop()
+if __name__ == "__main__":
+    app = BriefApp()
+    app.mainloop()
