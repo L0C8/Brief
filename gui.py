cbe6dfb9191d8d448a9c797ea33a2eefda89b447
L0C8 <TimB02@protonmail.com>
2025-04-15 03:10:04 +0200

    added api calls

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 from themes import get_current_theme, list_themes, switch_to_theme
+from scanner import scan_weather
 
 class BriefApp(tk.Tk):
     def __init__(self):
@@ -15,15 +16,14 @@ class BriefApp(tk.Tk):
         theme = get_current_theme()
         self.apply_theme(theme)
 
-        self.dropdown_label = tk.Label(self, text="Select Theme:")
-        self.dropdown_label.pack(pady=(20, 5))
-
-        self.theme_var = tk.StringVar(value=self.theme_names[0])
-        self.dropdown = ttk.Combobox(self, values=self.theme_names, textvariable=self.theme_var, state="readonly")
-        self.dropdown.pack(pady=(0, 20))
-        self.dropdown.bind("<<ComboboxSelected>>", self.on_theme_change)
+        self.test_button = tk.Button(self, text="Test", command=self.test_action)
+        self.test_button.pack(pady=(40, 10))
 
         self.update_widget_styles(theme)
+
+    def test_action(self):
+        weather = scan_weather("test")
+        print(weather)
 
     def on_theme_change(self, event):
         selected_name = self.theme_var.get()
@@ -38,8 +38,8 @@ class BriefApp(tk.Tk):
         self.update_widget_styles(theme)
 
     def update_widget_styles(self, theme):
-        if hasattr(self, "dropdown_label"):
-            self.dropdown_label.config(bg=theme["bg"], fg=theme["fg"])
+        if hasattr(self, "test_button"):
+            self.test_button.config(bg=theme["button_bg"], fg=theme["button_fg"])
         self.configure(bg=theme["bg"])
 
 if __name__ == "__main__":
